71d3f7d24cce543d1102fdbf420d3c1a354844c2
paul <111490417+Primz1@users.noreply.github.com>
2025-11-16 14:21:54 -0500

    Implement Daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import { WELCOME_EMAIL_TEMPLATE } from './templates';
+import { WELCOME_EMAIL_TEMPLATE, NEWS_SUMMARY_EMAIL_TEMPLATE } from '@/lib/nodemailer/templates';
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -7,21 +7,43 @@ export const transporter = nodemailer.createTransport({
         user: process.env.NODEMAILER_EMAIL!,
         pass: process.env.NODEMAILER_PASSWORD!,
     }
-}) 
-export const sendWelcomeEmail = async ({ email, name, intro} : WelcomeEmailData) => {
+})
+
+export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
     const htmlTemplate = WELCOME_EMAIL_TEMPLATE
-    .replace('{{name}}', name)
-    .replace('{{intro}}', intro);
+        .replace('{{name}}', name)
+        .replace('{{intro}}', intro);
 
     const mailOptions = {
-        from: ` "Signalist" <signalist@jsmastery.pro> `,
-        to : email,
-        subject: 'Welcome to Signalist!',
-        text: 'Thanks for joining Signalist.',
-        html: htmlTemplate, 
+        from: `"Signalist" <signalist@jsmastery.pro>`,
+        to: email,
+        subject: `Welcome to Signalist - your stock market toolkit is ready!`,
+        text: 'Thanks for joining Signalist',
+        html: htmlTemplate,
     }
 
     await transporter.sendMail(mailOptions);
 }
-    
-    
+
+type NewsSummaryEmailData = {
+    email: string;
+    date: string;
+    newsContent: string;
+};
+
+export const sendNewsSummaryEmail = async ({ email, date, newsContent }: NewsSummaryEmailData) => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: '"Signalist News" <signalist@jsmastery.pro>',
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: "Today's market news summary from Signalist",
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+
